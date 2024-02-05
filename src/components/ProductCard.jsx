11c79f04c3eb1d02a600b4fa4ab93d4427d3a916
suhaasvijay <suhaasvijay7@gmail.com>
2024-02-05 18:36:32 +0530

    add reloadDocument on link

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ export default function ProductCard(props) {
   const { product } = props;
 
   return (
-    <Link to={`/${product.id}`}>
+    <Link to={`/${product.id}`} reloadDocument>
       <div
         key={product.id}
         className="h-full hover:shadow-[0px_0px_20px_rgba(202,_108,_230,_0.6)]  transition-transform hover:scale-105 border border-gray-300 max-w-[360px]"
