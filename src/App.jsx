2477c5223a15386d4c25a3807706b647a9441731
suhaasvijay <suhaasvijay7@gmail.com>
2024-02-05 18:02:02 +0530

    Added loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import HomePage from "./pages/HomePage";
@@ -6,18 +7,35 @@ import ProductPage from "./pages/ProductPage";
 import SearchPage from "./pages/SearchPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import StartupLoader from "./components/StartupLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:id" element={<ProductPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <>
+      {loading ? (
+        <StartupLoader />
+      ) : (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:id" element={<ProductPage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
+    </>
   );
 }
 
---
+++ src/components/StartupLoader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function StartupLoader() {
+  return (
+    <div className="flex items-center content-center justify-center h-[90vh]">
+      <img src="/loading.gif" alt="loading" className="w-[200px] " />
+    </div>
+  );
+}
